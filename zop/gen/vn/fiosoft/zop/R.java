4022f34fe080f3ead14fb6d1072761bc3f7661e1
Phuong Bui <btphuong@acrobits.cz>
2012-10-22 11:55:55 +0700

    remove phonegap

--- zop/gen/vn/fiosoft/zop/R.java
+++ zop/gen/vn/fiosoft/zop/R.java
@@ -20,7 +20,8 @@ public final class R {
     }
     public static final class layout {
         public static final int activity_direction=0x7f030000;
-        public static final int activity_main=0x7f030001;
+        public static final int activity_login=0x7f030001;
+        public static final int activity_main=0x7f030002;
     }
     public static final class menu {
         public static final int activity_main=0x7f070000;

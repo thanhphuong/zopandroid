bf79c0096ca8939729439c3082bd109e91c66a2b
Phuong Bui <btphuong@acrobits.cz>
2012-10-22 17:54:52 +0700

    add new project of phonegap

---
+++ testphonegap/gen/vn/fiosoft/testphonegap/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package vn.fiosoft.testphonegap;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ testphonegap/src/vn/fiosoft/testphonegap/MyPhoneGapActivity.java
@@ -0,0 +1,12 @@
+package vn.fiosoft.testphonegap;
+
+import org.apache.cordova.DroidGap;
+import android.os.Bundle;
+
+public class MyPhoneGapActivity extends DroidGap {
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		super.loadUrl("file:///android_asset/www/index.html");
+	}
+}

192c90a5cf216c8ba3953285e19da9ac4e8617ac
Bui Thanh Phuong <btphuong@acrobits.cz>
2012-12-02 23:48:15 +0700

    bottom menu

--- zop/gen/vn/fiosoft/zop/R.java
+++ zop/gen/vn/fiosoft/zop/R.java
@@ -11,10 +11,15 @@ public final class R {
     public static final class attr {
     }
     public static final class drawable {
-        public static final int black_half_alpha=0x7f020000;
-        public static final int blue_half_alpha=0x7f020001;
-        public static final int ic_launcher=0x7f020002;
-        public static final int map_button_bg=0x7f020003;
+        public static final int action_search=0x7f020000;
+        public static final int black_half_alpha=0x7f020001;
+        public static final int blue_half_alpha=0x7f020002;
+        public static final int ic_action_overflow=0x7f020003;
+        public static final int ic_launcher=0x7f020004;
+        public static final int location_directions=0x7f020005;
+        public static final int location_map=0x7f020006;
+        public static final int location_place=0x7f020007;
+        public static final int map_button_bg=0x7f020008;
     }
     public static final class id {
         public static final int directions=0x7f070003;

25684e586de70588b75760d2580deb8f14c1d3be
Phuong Bui <btphuong@acrobits.cz>
2013-01-16 16:29:07 +0700

    delete version 1

--- zop/src/vn/fiosoft/common/Constants.java
+++
@@ -1,8 +0,0 @@
-package vn.fiosoft.common;
-
-public class Constants {
-	public static final int SHARE_LOCATION_ALL = 1;
-	public static final int SHARE_LOCATION_FRIEND = 2;
-	public static final int SHARE_LOCATION_GROUP = 3;
-
-}
